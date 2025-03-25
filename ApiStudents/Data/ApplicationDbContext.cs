27d790fabd0ca871bb1fc3e263f70ffb23d093bc
lkodovski <l.kodovski@hotmail.com>
2025-03-25 16:52:19 +0100

    commit all

---
+++ ApiStudents/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiStudents.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudents.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // Ovde se kreira DbSet
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
---
+++ ApiStudents/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiStudents.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Credits { get; set; }
+    }
+}
